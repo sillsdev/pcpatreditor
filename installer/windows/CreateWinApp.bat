c920a8a7ba12c09d87af3673fa457473b6760aea
andy black <blackhandrew@gmail.com>
2021-10-25 11:03:02 -0700

    Version 0.2.0 Alpha

--- installer/windows/CreateWinApp.bat
+++ installer/windows/CreateWinApp.bat
@@ -1,5 +1,5 @@
 @echo off
 if exist output rmdir output /S /q
 REM if exist apptemp rmdir apptemp /S /q
-jpackage --verbose --type app-image --input input --dest output --name PCPatrEditor --main-jar pcpatreditor.jar --main-class org.sil.pcpatreditor.MainApp --icon input/PcPatrEditor.ico --module-path "C:\Users\Andy Black\Favorites\Downloads\Java\AzulZulu\zulu14.29.23-ca-fx-jdk14.0.2-win_x64\jmods" --vendor "SIL International" --app-version 0.1.2
+jpackage --verbose --type app-image --input input --dest output --name PCPatrEditor --main-jar pcpatreditor.jar --main-class org.sil.pcpatreditor.MainApp --icon input/PcPatrEditor.ico --module-path "C:\Users\Andy Black\Favorites\Downloads\Java\AzulZulu\zulu14.29.23-ca-fx-jdk14.0.2-win_x64\jmods" --vendor "SIL International" --app-version 0.2.0
 call MoveResources.bat
