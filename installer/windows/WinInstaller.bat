ffc676e8139a9203d33b6cbed42b05feaef57896
andy black <blackhandrew@gmail.com>
2021-10-28 16:05:29 -0700

    Version 0.3.0

--- installer/windows/WinInstaller.bat
+++ installer/windows/WinInstaller.bat
@@ -1,4 +1,4 @@
 @echo off
 if exist installtemp rmdir installtemp /S /q
-jpackage --type exe --copyright "2021 SIL International" --description "Editor for PcPatr" --name PcPatrEditor --install-dir "SIL\PcPatrEditor" --resource-dir input/resources --app-image output/PcPatrEditor --win-menu --win-shortcut --license-file License.txt --temp installtemp --app-version 0.2.0 --vendor "SIL International"
+jpackage --type exe --copyright "2021 SIL International" --description "Editor for PcPatr" --name PcPatrEditor --install-dir "SIL\PcPatrEditor" --resource-dir input/resources --app-image output/PcPatrEditor --win-menu --win-shortcut --license-file License.txt --temp installtemp --app-version 0.3.0 --vendor "SIL International"
 
